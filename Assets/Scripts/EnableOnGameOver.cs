dd8222f740824a219dfbcaaeefcf99ad060f219e
William Crawford <wccrawford@gmail.com>
2013-08-24 17:42:22 -0400

    Added main menu and buttons for changing scenes.

---
+++ Assets/Scripts/EnableOnGameOver.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableOnGameOver : MonoBehaviour {
+	public GameObject[] objects;
+	public bool enable = true;
+	
+	void OnGameOver() {
+		for(int i = 0; i < objects.Length; i++) {
+			objects[i].SetActive(enable);
+		}
+	}
+}
---
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadSceneOnClick : MonoBehaviour {
+	public int sceneNumber = -1;
+	
+	void OnClick () {
+		if(sceneNumber == -1) {
+			Application.LoadLevel(Application.loadedLevel);
+		} else {
+			Application.LoadLevel(sceneNumber);
+		}
+	}
+}
