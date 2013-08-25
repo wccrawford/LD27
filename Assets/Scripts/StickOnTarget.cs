71d968c9b0acad0c63c06991576848367e6fc1ff
William Crawford <wccrawford@gmail.com>
2013-08-25 09:14:53 -0400

    Darts now stick to backboard.

---
+++ Assets/Scripts/StickOnTarget.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StickOnTarget : MonoBehaviour {
+	public string tagName = "Backboard";
+	
+	void OnTriggerEnter(Collider target) {
+		if(target.tag == tagName) {
+			transform.parent.rigidbody.isKinematic = true;
+		}
+	}
+}
