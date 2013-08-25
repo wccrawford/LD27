1bea13a580430c478facf52aa7914cda58bc7adb
William Crawford <wccrawford@gmail.com>
2013-08-25 15:03:15 -0400

    Added music.

---
+++ Assets/Scripts/SingletonObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingletonObject : MonoBehaviour {
+	public string singletonName;
+	
+	// Use this for initialization
+	void Start () {
+		GameObject[] gameObjects = GameObject.FindGameObjectsWithTag(singletonName);
+		if(gameObjects.Length > 1) {
+			Destroy(gameObject);
+			return;
+		}
+		DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
