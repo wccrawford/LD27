6b2b0399cb9f0c32d8edec6ba22bf032cf1e5213
William Crawford <wccrawford@gmail.com>
2013-08-25 16:06:57 -0400

    Added sound to throwables.

--- Assets/Scripts/DestroyOnTargetEnter.cs
+++ Assets/Scripts/DestroyOnTargetEnter.cs
@@ -2,14 +2,21 @@
 using System.Collections;
 
 public class DestroyOnTargetEnter : MonoBehaviour {
+	public AudioClip destroySound = null;
+	
 	private bool destroy = false;
 	
 	void Update() {
-		if(destroy) {
+		if(destroy && !audio.isPlaying) {
 			Destroy(gameObject);
 		}
 	}
 	void OnTargetEnter() {
+		if(destroySound != null) {
+			renderer.enabled = false;
+			audio.clip = destroySound;
+			audio.Play();
+		}
 		destroy = true;
 	}
 }
---
+++ Assets/Scripts/PlayWhileColliding.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayWhileColliding : MonoBehaviour {
+	void OnCollisionEnter(Collision collision) {
+		audio.Play();
+	}
+	
+	void OnCollisionExit(Collision collision) {
+		audio.Pause();
+	}
+	
+	void Update() {
+		if(rigidbody.IsSleeping() && audio.isPlaying) {
+			audio.Pause();
+		}
+	}
+}
